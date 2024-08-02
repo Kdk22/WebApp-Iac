fbc457e897c89311113edaa352125c9883d1ca5c
Utshab <khadkautshav7@gmail.com>
2024-08-01 22:20:05 -0400

    main file edited for state file storage config

--- main.tf
+++ main.tf
@@ -5,7 +5,15 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+  # here i am storing the state file 
+   backend "azurerm" {
+    resource_group_name  = "myTFResourceGroup"
+    storage_account_name = "statestoragetf"
+    container_name       = "statecon"
+    key                  = "terraform.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   features {}
