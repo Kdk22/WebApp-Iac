9d10f1b1e527aa9a82c778f40dee4aed81a9d132
Utshab <khadkautshav7@gmail.com>
2024-08-04 22:22:52 -0400

    Added logging diagonise  for key valut
    - removed not used access policy from key vault
    - added azure monitor diagnostic settings for key valut logging

--- keyvault.tf
+++ keyvault.tf
@@ -15,12 +15,12 @@ resource "azurerm_key_vault" "fg-keyvault" {
 
 }
 
-
+# this permission is for service connection from app registration, this is given to store database secrets to key vault
 resource "azurerm_key_vault_access_policy" "kv_access_policy_01" {
-  #This policy adds databaseadmin group with below permissions
+
   key_vault_id = azurerm_key_vault.fg-keyvault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = "0aa40ef3-115e-4628-826d-ab7f53bd396f"
+  object_id    = "54b84d5e-84a5-49fc-8c76-f8d0604cc8c1"
   key_permissions = [
     "Get", "List"
   ]
@@ -33,7 +33,6 @@ resource "azurerm_key_vault_access_policy" "kv_access_policy_01" {
 
 # permission to my self
 resource "azurerm_key_vault_access_policy" "kv_access_policy_02" {
-  #This policy adds databaseadmin group with below permissions
   key_vault_id       = azurerm_key_vault.fg-keyvault.id
   tenant_id          = data.azurerm_client_config.current.tenant_id
   object_id          = "ab35c899-90de-4dcc-9d4f-ebae7a569976"
@@ -44,33 +43,39 @@ resource "azurerm_key_vault_access_policy" "kv_access_policy_02" {
 }
 
 
-resource "azurerm_key_vault_access_policy" "kv_access_policy_03" {
-  #This policy adds databaseadmin group with below permissions
-  key_vault_id       = azurerm_key_vault.fg-keyvault.id
-  tenant_id          = data.azurerm_client_config.current.tenant_id
-  object_id          = "54b84d5e-84a5-49fc-8c76-f8d0604cc8c1"
-  key_permissions    = ["Get", "List"]
-  secret_permissions = ["Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set"]
 
-  depends_on = [azurerm_key_vault.fg-keyvault]
-}
+# need to enable the logging for key vault
+# here i used the same storge accout created for function app: azurerm_linux_function_app
+# if you use   log_analytics_destination_type = Dedicated then your cost would be higher
 
-/*
-need to enable the logging for key vault so 
 resource "azurerm_monitor_diagnostic_setting" "kvlogging" {
-  name               = "kvlogging-diagonise"
-  target_resource_id = azurerm_key_vault.fg-keyvault.id
-  storage_account_id = azurerm_storage_account.task1-storage.id
+  name                       = "kvlogging-diagonise"
+  target_resource_id         = azurerm_key_vault.fg-keyvault.id
+  storage_account_id         = azurerm_storage_account.fn-storageaccount.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.fg-loganalytics.id
-  log_analytics_destination_type = Dedicated
 
-  metric {
-    category = "AllMetrics"
+
+  log {
+    category = "AuditEvent"
+    enabled  = true
 
     retention_policy {
       enabled = false
+      days    = 0
     }
   }
-  depends_on = [ azurerm_storage_account.task1-storage ]
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+
+  }
+  depends_on = [
+    azurerm_storage_account.fn-storageaccount
+  ]
 }
-*/
